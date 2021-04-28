a5d19ec785fbb95b603e38ae6567d20e04720d75
gevorgrashoyan <gevorgrashoyan>
2021-04-28 21:40:05 +0400

    Adding the updated version

--- C/SumOfFibNum.c
+++ C/SumOfFibNum.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <string.h>
 int calculateSum(int n)
 {
         int iM2 = 0, iM1 = 1, fib, sum;
@@ -17,14 +18,29 @@ int calculateSum(int n)
         return sum;
 }
 
+void printBinary(char *msg, int n) {
+        char binaryBuf[33];
+        int i, d, j = 0;
+
+        for (i = 31; (i >= 0) && ! (n >> i); i--);
+        while (i >= 0) {        
+            d = n >> i--;
+            binaryBuf[j++] = ((d & 1) ? 1 : 0) + '0';
+        }
+        binaryBuf[j] = '\0';
+        printf("%s %s\n", msg, (j ==0) ? "0" : binaryBuf);
+}
+
 
 int main ()
 {
-   int n;
+   int n, sum;
 
    printf("Enter the integer: ");
    scanf("%d", &n);
-   printf("Sum of Fibonacci numbers is %d", calculateSum(n));
+   sum = calculateSum(n);
+   printf("Sum of Fibonacci numbers is %d\n", sum);
+   printBinary("Binary sum of Fibonacci numbers is", sum);
    getchar();
    return 0;
 }
